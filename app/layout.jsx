fa8435451006be97384ceecdbb995125fea5cef1
Marija Todorovic <maja.todorovic2111@gmail.com>
2025-07-04 14:42:37 +0200

    Izmene test+

--- app/layout.jsx
+++ app/layout.jsx
@@ -51,10 +51,30 @@ export default function RootLayout({ children }) {
           src="https://kit.fontawesome.com/f141ac3909.js"
         />
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+          {/* GTM script - ubaci ovde */}
+          <Script id="gtm-script" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+
+})(window,document,'script','dataLayer','GTM-56MFRTQF');
+          `}
+        </Script>
       </head>
       <body
         className={`${old_standard?.variable} ${century_schoolbook?.variable} !min-h-dvh`}
       >
+         {/* GTM noscript - ubaci odmah posle otvaranja body */}
+         <noscript>
+         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-56MFRTQF"
+
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
         <CartContextProvider>
           <UserProvider>
             <QueryProvider>
